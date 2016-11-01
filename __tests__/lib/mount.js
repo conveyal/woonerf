431ce8a31a77dbc54e2c15c8608833d67633403a
Trevor Gerhardt <trevorgerhardt@gmail.com>
2016-11-01 18:17:49 +0700

    test(mount): Fix mount test on travis (hopefully)

--- __tests__/lib/mount.js
+++ __tests__/lib/mount.js
@@ -4,8 +4,6 @@ import React, {Component, PropTypes} from 'react'
 import {mount} from 'enzyme'
 import {mountToJson} from 'enzyme-to-json'
 
-import {create} from '../../lib/mount'
-
 class App extends Component {
   static propTypes = {
     history: PropTypes.object.isRequired
@@ -18,6 +16,8 @@ class App extends Component {
 
 describe('mount', () => {
   it('should mount the react application to root', () => {
+    process.env.NODE_ENV = 'test'
+    const create = require('../../lib/mount').create
     const tree = mount(create({app: App, reducers: {}}))
     expect(mountToJson(tree)).toMatchSnapshot()
   })
